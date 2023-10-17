4c90e09d1dbf89b7dcf1502bdf6240835fccd596
TLP-242 <ilay.gussarsky.44@gmail.com>
2023-10-17 16:19:54 +0300

    Added base site

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('submit-button').addEventListener('click', function() {
+  const text = document.getElementById('text-input').value;
+  // Call a function to send this text to Google Sheets API here
+});
